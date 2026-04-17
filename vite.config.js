01484bbe8eef2b32c441477c86278defc085c436
eloise dinh <nhidinh2@illinois.edu>
2026-04-17 16:40:52 -0500

    base back to cs341

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import { resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
+export default defineConfig({
+  base: "./",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+        probability: resolve(__dirname, "probability.html"),
+        "statistical-inference": resolve(__dirname, "statistical-inference.html"),
+        "statistical-models-and-methods": resolve(
+          __dirname,
+          "statistical-models-and-methods.html"
+        ),
+      },
+    },
+  },
+});
